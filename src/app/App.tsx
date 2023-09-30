9c433096fcc8fb4c73df2cf2688e4a6b8eae6911
Daijoubu22 <alextrio.2224@gmail.com>
2023-09-30 19:57:19 +0300

    feat: add nextui

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,12 @@
 import { FamilyTree } from '@/pages/FamilyTree'
 import './styles/index.css'
+import { NextUIProvider } from '@nextui-org/react'
 
 function App() {
   return (
-    <FamilyTree />
+    <NextUIProvider>
+      <FamilyTree />
+    </NextUIProvider>
   )
 }
 
